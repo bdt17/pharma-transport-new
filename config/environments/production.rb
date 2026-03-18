5d8abebe91ebfb9455c8ca24829ec1f7f278036e
bdt17 <brett.thomas29.97@gmail.com>
2026-03-17 18:02:32 -0700

    fix: bulletproof render production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,21 +3,16 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? == true
-
-  config.log_level = :info
+  
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES']
+  
+  config.log_level = :warn
   config.logger = ActiveSupport::Logger.new(STDOUT)
-  config.log_tags = [:request_id]
-
+  
   config.action_mailer.perform_caching = false
-  config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
-
   config.active_record.sqlite3_production_warning = false
   config.active_job.queue_adapter = :inline
   config.force_ssl = true
   
-  config.hosts << "render.com"
   config.hosts << ".onrender.com"
 end
