45998bfb200203c3d3fb5cf580d103e8f797b709
bdt17 <brett.thomas29.97@gmail.com>
2026-03-18 11:34:46 -0700

    fix: add /pay route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get '/health', to: 'health#show'
   get '/pay', to: 'checkout#show'
-  get '/success', to: 'checkout#success'
+  get '/success', to: 'checkout#success'  
   get '/dashboard', to: 'dashboard#index'
-  
   root 'health#show'
 end
