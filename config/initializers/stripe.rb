9d78244b4f86590c808a7cba39beed698da60f14
bdt17 <brett.thomas29.97@gmail.com>
2026-03-20 16:45:36 -0700

    set real Stripe keys in credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,9 @@
+# config/initializers/stripe.rb
+
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.credentials.stripe[:publishable_key],
-  secret_key:      Rails.application.credentials.stripe[:secret_key],
-  webhook_secret:  Rails.application.credentials.stripe[:webhook_secret]
+  publishable_key: Rails.application.credentials.dig(:stripe, :publishable_key),
+  secret_key:      Rails.application.credentials.dig(:stripe, :secret_key),
+  webhook_secret:  Rails.application.credentials.dig(:stripe, :webhook_secret)
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
