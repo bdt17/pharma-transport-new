48f055c78b5f5ca44f1c1a0bca501dd96f9b2e8a
bdt17 <brett.thomas29.97@gmail.com>
2026-03-24 11:04:24 -0700

    FIX: root to pages#index + render layout false

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    render layout: false  # ← ADD THIS LINE
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  root "application#index"
+  root "pages#index"  # Fixed!
+  
   get '/batches/:id/chain-of-custody.pdf', to: 'batches#show'
   
-  get "/batches", to: proc { 
+  get "/batches", to: proc {
     [200, {"Content-Type" => "text/plain"}, ["Batches page coming soon..."]]
   }
-  
-  get "/health", to: proc { 
-    [200, {"Content-Type" => "application/json"}, 
+
+  get "/health", to: proc {
+    [200, {"Content-Type" => "application/json"},
      [{ok: true, service: "pharma-transport", ts: Time.now.utc.iso8601}.to_json]]
   }
 end
