2f02db681a3c91923e897ef23484e6b8b28cad32
bdt17 <brett.thomas29.97@gmail.com>
2026-03-24 14:29:30 -0700

    v1.5 Login button in nav + PDF routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root "pages#index"
   
-  # Dashboard + Batch routes (NEW)
+  # Dashboard + Batch routes
   get '/batches', to: 'batches#index', as: :batches
   get '/batches/:id', to: 'batches#show', as: :batch
-  get '/batches/:id/chain-of-custody.pdf', to: 'batches#show', as: :batch_pdf
+  get '/batches/:id/chain-of-custody.pdf', to: 'batches#show', as: :batch_pdf  # ← CRITICAL
   
-  # Health check (KEEP)
-  get "/health", to: proc {
-    [200, {"Content-Type" => "application/json"},
+  get "/health", to: proc do
+    [200, {"Content-Type" => "application/json"}, 
      [{ok: true, service: "pharma-transport", ts: Time.now.utc.iso8601}.to_json]]
-  }
+  end
 end
