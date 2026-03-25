2351d1e44b8e3b18a52b348317b9fe5fd0484ba9
bdt17 <brett.thomas29.97@gmail.com>
2026-03-24 19:47:28 -0700

    v1.7 Routes fixed - chain_of_custody_batch PDF ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root "pages#index"
   
-  # Dashboard + Batch routes
-  get '/batches', to: 'batches#index', as: :batches
-  get '/batches/:id', to: 'batches#show', as: :batch
-  get '/batches/:id/chain-of-custody.pdf', to: 'batches#show', as: :batch_pdf  # ← CRITICAL
-  
-  get "/health", to: proc do
-    [200, {"Content-Type" => "application/json"}, 
-     [{ok: true, service: "pharma-transport", ts: Time.now.utc.iso8601}.to_json]]
+  resources :batches, only: [:index, :show] do
+    member do
+      get :chain_of_custody, path: 'chain-of-custody', defaults: {format: 'pdf'}
+    end
   end
+  
+  get "/health", to: proc { 
+    [200, {"Content-Type" => "application/json"}, [{ok: true, service: "pharma-transport", ts: Time.now.utc.iso8601}.to_json]] 
+  }
 end
