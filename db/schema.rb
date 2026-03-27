c871f63db1245a75f251c051082156b89f0b4778
bdt17 <brett.thomas29.97@gmail.com>
2026-03-26 21:33:17 -0700

    FIX: schema.rb for Render Postgres - skip local SQLite conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_20_040750) do
-  create_table "shipments", force: :cascade do |t|
-    t.string "shipment_id"
-    t.string "status"
-    t.text "biologics"
-    t.string "origin"
-    t.string "destination"
-    t.string "temperature"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "encrypted_otp_secret"
-    t.text "otp_backup_codes"
-    t.integer "consumed_timestep"
-    t.string "name"
-    t.datetime "current_sign_in_at"
-    t.integer "sign_in_count"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "last_sign_in_at"
-    t.integer "failed_attempts"
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
