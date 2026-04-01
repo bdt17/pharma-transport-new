8ca5dbf9e26089aebece50d7fb3f6842938aa94b
bdt17 <brett.thomas29.97@gmail.com>
2026-04-01 12:13:28 -0700

    Remove broken Devise monkey patch

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,10 @@
 require_relative "boot"
-
 require "rails/all"
-
 Bundler.require(*Rails.groups)
 
 module PharmaTransportClean
   class Application < Rails::Application
     config.load_defaults 7.1
-
-    # Minimal config - no custom railties
     config.active_record.async_query_executor = :global_thread_pool
   end
 end
