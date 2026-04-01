f1c6cd2994db5bbb1f87a1ae88887cb80fa0656e
bdt17 <brett.thomas29.97@gmail.com>
2026-04-01 12:06:43 -0700

    REVERT: Remove broken Devise monkey patch

--- config/application.rb
+++ config/application.rb
@@ -7,19 +7,8 @@ Bundler.require(*Rails.groups)
 module PharmaTransportClean
   class Application < Rails::Application
     config.load_defaults 7.1
-    
+
     # Minimal config - no custom railties
     config.active_record.async_query_executor = :global_thread_pool
-  end
-end
-
-# Silence Devise Rack deprecation noise
-module Devise
-  module FailureApp
-    def respond
-      super
-    rescue Warning => e
-      nil if e.message.include?("unprocessable_entity")
-    end
   end
 end
