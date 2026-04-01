0e58cffdc11dbc4445721d63075a7509485da7d0
bdt17 <brett.thomas29.97@gmail.com>
2026-04-01 11:57:41 -0700

    Fix Rack unprocessable_entity deprecation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+   config.responder.error_status = :unprocessable_content
+   config.responder.redirect_status = :see_other
+
   config.warden do |manager|
     manager.default_strategies(:scope => :user).unshift :two_factor_authenticatable
   end
