13c77e9b978d115e9cbed91a7d7885035116b8b9
bdt17 <brett.thomas29.97@gmail.com>
2026-04-11 20:47:36 -0700

    Simplify Stripe to pure ENV

--- src/config/initializers/stripe.rb
+++ src/config/initializers/stripe.rb
@@ -1,3 +1,1 @@
-Rails.application.config.to_prepare do
-  Stripe.api_key = ENV['STRIPE_SECRET_KEY'] || Rails.application.credentials.dig(:stripe, :secret_key)
-end
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
