ce531e7ad79d540845e748f5409ea4ddd09f1b1c
bdt17 <brett.thomas29.97@gmail.com>
2026-04-12 18:03:30 -0700

    Lazy load Stripe.api_key post-initializers

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,12 +1,9 @@
-# config/initializers/stripe.rb
-
 Rails.configuration.stripe = {
   publishable_key: Rails.application.credentials.dig(:stripe, :publishable_key),
   secret_key:      Rails.application.credentials.dig(:stripe, :secret_key),
   webhook_secret:  Rails.application.credentials.dig(:stripe, :webhook_secret)
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-
-
+Rails.application.config.after_initialize do
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+end
